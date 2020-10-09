f0e46aa19a7ae991849da33035534b0c95d73ecc
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2020-10-09 10:07:40 +0300

    Remove dummy log

--- RtspClientSharp/MediaParsers/OnvifMetaDataPayloadParser.cs
+++ RtspClientSharp/MediaParsers/OnvifMetaDataPayloadParser.cs
@@ -9,17 +9,13 @@ namespace RtspClientSharp.MediaParsers
 
         public void Parse(TimeSpan timeOffset, ArraySegment<byte> byteSegment, bool markerBit)
         {
-            var datA = System.Text.Encoding.UTF8.GetString(byteSegment.Array);
-            System.Console.WriteLine("parse -------------");
-            System.Console.WriteLine("DATA: " + datA + "\n++++++++++++++++++++++");
-
             var frame = new RawMetaDataFrame(DateTime.UtcNow, byteSegment);
             FrameGenerated?.Invoke(frame);
         }
 
         public void ResetState()
         {
-            System.Console.WriteLine("reset -------------");
+            System.Console.WriteLine("------------- OnvifMetaDataPayloadParser: RESET -------------");
         }
     }
 }
